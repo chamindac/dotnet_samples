f1c9fd7492c2dd660626d108b87046ac96c6fd1a
Chaiminda Chandrasekara <chaminda.chandrasekara@ifs.com>
2024-10-31 16:48:53 +0100

    generate SaS uri with default azure creds

---
+++ GenerateBlobSaSUri/Program.cs
@@ -0,0 +1,46 @@
+﻿using Azure.Identity;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs;
+using Azure.Storage.Sas;
+
+namespace GenerateBlobSaSUri;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        BlobServiceClient blobServiceClient = new(
+    new Uri("https://cheuw001assetssthot.blob.core.windows.net/"),
+    new DefaultAzureCredential());
+
+        BlobContainerClient blobContainer = blobServiceClient.GetBlobContainerClient("dotnet-1e049c87-ce56-4c54-afc8-0c5a01a97bf3");
+        BlobClient blob = blobContainer.GetBlobClient("boatnewyork_005.png");
+        DateTimeOffset startsOn = DateTimeOffset.UtcNow;
+        DateTimeOffset expiresOn = startsOn.AddHours(1);
+
+        UserDelegationKey userDelegationKey = await blobServiceClient.GetUserDelegationKeyAsync(
+            startsOn, expiresOn);
+
+        BlobSasBuilder sasBuilder = new()
+        {
+            BlobContainerName = blob.BlobContainerName,
+            BlobName = blob.Name,
+            Resource = "b",
+            StartsOn = startsOn,
+            ExpiresOn = expiresOn
+        };
+
+        sasBuilder.SetPermissions(BlobSasPermissions.Read);
+
+        BlobUriBuilder blobUriBuilder = new(blob.Uri)
+        {
+            Sas = sasBuilder.ToSasQueryParameters(
+                    userDelegationKey,
+                    blobServiceClient.AccountName)
+        };
+
+        Uri uri = blobUriBuilder.ToUri();
+
+        Console.WriteLine(uri.AbsoluteUri);
+    }
+}
