a657098d687e1cc83805a716a15e6af128027d6d
Chaiminda Chandrasekara <chaminda.chandrasekara@ifs.com>
2025-08-31 14:35:03 +0200

    di enabled sample console

---
+++ CosmosWithDefaultAzureCreds/di_sample/App.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace di_sample
+{
+    public class App
+    {
+        private readonly ILogger<App> _logger;
+
+        public App(ILogger<App> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task RunAsync()
+        {
+            _logger.LogInformation("Application started");
+            _logger.LogInformation("Hello, Dependency Injection!");
+            _logger.LogInformation("Application finished");
+
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ CosmosWithDefaultAzureCreds/di_sample/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace di_sample
+{ 
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            ServiceCollection services = new();
+
+            services.AddLogging(loggingBuilder =>
+            {
+                loggingBuilder.ClearProviders();
+                loggingBuilder.AddSimpleConsole(options =>
+                {
+                    options.IncludeScopes = true;
+                });
+            });
+
+            ConfigureServices(services);
+
+            using (var serviceProvider = services.BuildServiceProvider())
+            {
+                App app = serviceProvider.GetRequiredService<App>();
+                await app.RunAsync();
+            }
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<App>();
+            //services
+            //    .AddScoped<ITranscoder, Transcoder>()
+            //    .AddSingleton<IRabbitMQConnectionProvider, RabbitMQConnectionProvider>()
+            //    .AddSingleton<IRabbitMQConsumerProvider, RabbitMQConsumerProvider>()
+            //    .AddSingleton<IRabbitMQMessageHandler, VideoGenerationRequiredHandler>()
+            //    .AddSingleton<IRabbitMQMessageHandler, RegenerateVideoHandler>()
+            //    .AddSingleton<IRabbitMQMessageHandler, ExtractMetadataHandler>()
+            //    .AddHostedService<RabbitMQHostedService>();
+        }
+    }
+}
